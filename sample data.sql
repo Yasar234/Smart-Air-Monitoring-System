b94faf93c87522ba1918d8bdedd084904dbea2d1
yasar <147140268+Yasar234@users.noreply.github.com>
2024-09-06 10:10:54 +0530

    sample data.sql

--- sample data.sql
+++ sample data.sql
@@ -1,20 +1,46 @@
 INSERT INTO Sensors (sensor_name, sensor_type, status)
 VALUES 
-('Main Office Gas Sensor', 'Gas', 'Active'),
-('Living Room Humidity Sensor', 'Humidity', 'Active'),
-('Warehouse Gas Sensor', 'Gas', 'Active'),
-('Bedroom Humidity Sensor', 'Humidity', 'Inactive');
-
+(@sensor_name1, @sensor_type1, @status1),
+(@sensor_name2, @sensor_type2, @status2),
+(@sensor_name3, @sensor_type3, @status3),
+(@sensor_name4, @sensor_type4, @status4);
+SET @sensor_name1 = 'Main Office Gas Sensor';
+SET @sensor_type1 = 'Gas';
+SET @status1 = 'Active';
+SET @sensor_name2 = 'Living Room Humidity Sensor';
+SET @sensor_type2 = 'Humidity';
+SET @status2 = 'Active';
+SET @sensor_name3 = 'Warehouse Gas Sensor';
+SET @sensor_type3 = 'Gas';
+SET @status3 = 'Active';
+SET @sensor_name4 = 'Bedroom Humidity Sensor';
+SET @sensor_type4 = 'Humidity';
+SET @status4 = 'Inactive';
 INSERT INTO GasSensorData (sensor_id, gas_level)
 VALUES 
-(1, 405.5),  -- Corresponding to 'Main Office Gas Sensor'
-(1, 412.3),  -- Corresponding to 'Main Office Gas Sensor'
-(3, 350.8),  -- Corresponding to 'Warehouse Gas Sensor'
-(3, 368.9);  -- Corresponding to 'Warehouse Gas Sensor'
-
+(@sensor_id1, @gas_level1),  
+(@sensor_id1, @gas_level2),  
+(@sensor_id3, @gas_level3),  
+(@sensor_id3, @gas_level4);  
+SET @sensor_id1 = 1;
+SET @gas_level1 = 405.5;
+SET @gas_level2 = 412.3;
+SET @sensor_id3 = 3;
+SET @gas_level3 = 350.8;
+SET @gas_level4 = 368.9;
 INSERT INTO HumiditySensorData (sensor_id, humidity, temperature)
 VALUES 
-(2, 55.4, 22.1),  -- Corresponding to 'Living Room Humidity Sensor'
-(2, 53.7, 21.9),  -- Corresponding to 'Living Room Humidity Sensor'
-(4, 60.2, 19.3),  -- Corresponding to 'Bedroom Humidity Sensor'
-(4, 58.6, 18.8);  -- Corresponding to 'Bedroom Humidity Sensor'
+(@sensor_id2, @humidity1, @temperature1),  
+(@sensor_id2, @humidity2, @temperature2),  
+(@sensor_id4, @humidity3, @temperature3),  
+(@sensor_id4, @humidity4, @temperature4);  
+SET @sensor_id2 = 2;
+SET @humidity1 = 55.4;
+SET @temperature1 = 22.1;
+SET @humidity2 = 53.7;
+SET @temperature2 = 21.9;
+SET @sensor_id4 = 4;
+SET @humidity3 = 60.2;
+SET @temperature3 = 19.3;
+SET @humidity4 = 58.6;
+SET @temperature4 = 18.8;
